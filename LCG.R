f619fed3244b8f2cb6b211b86049e03c6b6b820b
vicjuh1212 - macbook <victoriasmit@hotmail.nl>
2020-06-16 09:01:24 +0200

    v2. ICDF exponential, ICDF Normal

--- LCG.R
+++ LCG.R
@@ -5,7 +5,7 @@ m <- 2 ** 32
 a <- 1103515245
 c <- 12345
 
-## Function
+#### Function LCG ####
 LCG <- function(n, x0=2000, a=a, c=c, m=m){
   lcg <- vector(length=n)
   d <- x0
@@ -16,5 +16,30 @@ LCG <- function(n, x0=2000, a=a, c=c, m=m){
   return(lcg)
 }
 
-## Testing parameters
+#### Testing parameters####
 LCG(10)
+
+
+#### Random inv exp ####
+ICDF_exp <- function(n, lambda=200){
+  u <- runif(n)
+  return((- lambda * log(1-u)))
+}
+hist(ICDF_exp(100))
+
+#### INV NORMAL ####
+ICDF_norm <- function(n){
+  output <- vector(length=n)
+  for (i in 1:(n)){
+  start <- runif(2)
+  u1 <- start[1]
+  u2 <- start[2]
+  z1 <- sqrt(-2*log(u1))*cos(2*pi*u2)
+  z2 <- sqrt(-2*log(u1))*sin(2*pi*u2)
+  output[i] <- c(z1)
+  output[i+1]}
+  
+  return(output)
+}
+hist(ICDF_norm(100000))
+
